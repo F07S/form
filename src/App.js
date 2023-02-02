9444eab088e0ba59fe61e9804dc719a29c03d5d0
Francesca Saunders <francescasaunders@MBPdeFrancesca.lan>
2023-02-02 16:57:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // Input States
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  // Error Message State
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // Result State
+  const [result, setResult] = useState("");
+
+  // onChange Input Handlers
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        className="form-box"
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          // condition identical passwords
+          if (password !== confirmPassword) {
+            alert("Your passwords don't match.");
+          } else {
+            document.querySelector("#create").classList.toggle("disappear") &&
+              document.querySelector("#results").classList.add("appear");
+          }
+        }}
+      >
+        <div id="create">
+          <h1>Create Account</h1>
+          <div>
+            <span>Name</span>
+            <input
+              value={name}
+              type="text"
+              placeholder="name"
+              onChange={handleNameChange}
+            />
+          </div>
+          <div>
+            <span>Email</span>
+            <input
+              value={email}
+              type="email"
+              placeholder="email"
+              onChange={handleEmailChange}
+            />
+          </div>
+          <div>
+            <span>Password</span>
+            <input
+              value={password}
+              type="password"
+              placeholder="password"
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div>
+            <span>Confirm your password</span>
+            <input
+              value={confirmPassword}
+              type="password"
+              placeholder="confirm password"
+              onChange={handleConfirmPasswordChange}
+            />
+          </div>
+          <button type="submit">Register</button>
+        </div>
+      </form>
+      <section id="results">
+        <h1>Results</h1>
+        <div>{name}</div>
+        <div>{email}</div>
+        <div>{password}</div>
+        <button>Edit your information</button>
+      </section>
     </div>
   );
 }
